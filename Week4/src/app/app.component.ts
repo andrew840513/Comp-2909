b673c09b6e2d5de4ca453daf4fed10a6b2265001
Andrew Chen <andrew@mren.com.tw>
2017-05-20 23:16:12 -0700

    Revert "update"
    This reverts commit a1fb24ad630ed8a369268cb6c46f6f90dc720f0e.

--- Week4/src/app/app.component.ts
+++ Week4/src/app/app.component.ts
@@ -1,46 +1,28 @@
 import { Component } from '@angular/core';
-import {  MyNameService } from './app.mynameservice';
+import { MyDataService } from './myDataService';
+import { TemperatureService} from './temperatureService';
 
-// This component consumes the re-usable service.
 @Component({
   selector: 'my-app',
-  template: `<button (click)="getSomeData()">Test GET Request</button>
-  <ul>
-    <li *ngFor="let myData of myNames">{{myData.first}} {{myData.last}}</li>
-  </ul>`,
-  // Providers allow us to inject an object instance through the constroctor.
-  // In this case we enable injection of MyDataService into AppComponent.
-  providers: [MyNameService]
+  template: `<h1>Hello world!  {{title}}</h1>
+  <ul><li *ngFor="let name of names">{{name}}</li></ul>
+  <p>My name: {{myName}}</p>
+  <p>6 degree celsius to fahrenheit:{{celsius}}</p>`,
+  // 'providers' allows you to create and pass an instance
+  // of the class to the constructor header.
+  providers: [MyDataService, TemperatureService]
 })
 export class AppComponent {
-  myNames: Array<any>;
-  _myDataService: MyNameService;
-
-  // Since we are using a provider above we can receive
-  // an instance through an instructor.
-  constructor(myDataService: MyNameService) {
-    // Store local reference to MyDataService.
-    this._myDataService = myDataService;
-  }
-
-  getSomeData() {
-    this._myDataService.getNames()
-    // Subscribe to changes in the observable object
-    // that is returned by getRemoteData.
-      .subscribe(
-        // You basically get three handlers.
-        // 1. Handle successful data.
-        data => {
-          this.myNames = data
-          console.log(JSON.stringify(data))
-        },
-        // 2. Handle error.
-        error => {
-          alert(error)
-        },
-        // 3. Execute final instructions when successful.
-        () => {
-          console.log("Finished")
-        });
+  public title = 'This is Angular2!';
+  names: Array<any>;
+  myName: String;
+  celsius: number;
+  // Create instance of 'MyDataService' right in the constructor
+  // header.
+  constructor(myDataService: MyDataService, temperatureService: TemperatureService) {
+    // Use service to call getNames() method.
+    this.names = myDataService.getNames();
+    this.myName = myDataService.getMyName();
+    this.celsius = temperatureService.converFromCToF(6);
   }
 }
